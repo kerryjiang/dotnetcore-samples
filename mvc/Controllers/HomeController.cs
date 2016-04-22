c955df589a401db69da0c246ee3f928d2dbe42c7
Kerry Jiang <kerry-jiang@hotmail.com>
2016-04-22 12:00:21 +0800

    added mvc sample

---
+++ mvc/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvc
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvc/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HelloMvc
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDirectoryBrowser();
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            
+            app.UseStaticFiles();
+            //app.UseDirectoryBrowser();
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseMvc();
+        }
+    }
+}
